f85adee1a27182876144b7cbf20adc88ec823972
Jacob Angulo <jacobangulo@hotmail.com>
2019-03-01 08:56:37 -0800

    clicking a smurf will take you to that smurfs page

--- village/src/App.js
+++ village/src/App.js
@@ -1,43 +1,44 @@
-import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
-import axios from 'axios'
+import React, { Component } from "react";
+import { Route, NavLink } from "react-router-dom";
+import axios from "axios";
 
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
+import Smurf from "./components/Smurf";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
   componentDidMount() {
     axios
-      .get('http://localhost:3333/smurfs')
+      .get("http://localhost:3333/smurfs")
       .then(res => {
         this.setState({
           smurfs: res.data
         });
-        console.log(res)
+        console.log(res);
       })
-      .catch(err => console.log('ERR', err))
+      .catch(err => console.log("ERR", err));
   }
 
   addSmurf = (e, newSmurf) => {
-    e.preventDefault()
+    e.preventDefault();
     axios
-      .post('http://localhost:3333/smurfs', newSmurf)
+      .post("http://localhost:3333/smurfs", newSmurf)
       .then(res => {
         this.setState({
           smurfs: res.data
-        })
-        console.log(res)
-        this.props.history.push('/')
+        });
+        console.log(res);
+        this.props.history.push("/");
       })
-      .catch(err => console.log('ERR', err))
-  }
+      .catch(err => console.log("ERR", err));
+  };
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
@@ -45,11 +46,22 @@ class App extends Component {
     return (
       <div className="App">
         <nav>
-          <NavLink to='/'>smurf list</NavLink>
-          <NavLink to='/smurf-form'>add smurf</NavLink>
+          <NavLink to="/">smurf list</NavLink>
+          <NavLink to="/smurf-form">add smurf</NavLink>
         </nav>
-        <Route exact path='/' render={() => <Smurfs smurfs={this.state.smurfs}/>} />
-        <Route path='/smurf-form' render={() => <SmurfForm addSmurf={this.addSmurf}/>} />
+        <Route
+          exact
+          path="/"
+          render={() => <Smurfs smurfs={this.state.smurfs} />}
+        />
+        <Route
+          path="/smurf-form"
+          render={() => <SmurfForm addSmurf={this.addSmurf} />}
+        />
+        <Route
+          path="/smurfs/:id"
+          render={props => <Smurf {...props} smurfs={this.state.smurfs} />}
+        />
       </div>
     );
   }
--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Smurf = props => {
+  const id = props.match.params.id;
+  const smurf = props.smurfs.find(smurf => smurf.id === parseInt(id))
   return (
     <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
+      <h3>{smurf.name}</h3>
+      <strong>{smurf.height} tall</strong>
+      <p>{smurf.age} smurf years old</p>
     </div>
   );
 };
